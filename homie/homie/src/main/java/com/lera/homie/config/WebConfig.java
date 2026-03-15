4f1e59db863cf7804d004070cae62e6167b608d9
Valeriia Minchuk <leravaleriia200518@gmail.com>
2026-03-15 16:02:33 +0100

    add temporary template

--- homie/homie/src/main/java/com/lera/homie/config/WebConfig.java
+++ homie/homie/src/main/java/com/lera/homie/config/WebConfig.java
@@ -3,6 +3,10 @@ package com.lera.homie.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class WebConfig {
@@ -15,4 +19,13 @@ public class WebConfig {
         return resolver;
     }
 
+    @Bean
+    public DataSource dataSource() {
+        return new DriverManagerDataSource();
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
 }
