7412c5c8f919dcd8332a993f236e62b7f5a1f31b
Valeriia Minchuk <leravaleriia200518@gmail.com>
2026-03-16 01:42:05 +0100

    changes db

--- homie/homie/src/main/resources/schema.sql
+++ homie/homie/src/main/resources/schema.sql
@@ -1,8 +1,8 @@
-CREATE TABLE IF NOT EXISTS usuarios (
+CREATE TABLE IF NOT EXISTS usuario (
     id SERIAL PRIMARY KEY,
     email VARCHAR(100),
     password VARCHAR(100)
     );
 
-INSERT INTO usuarios (email, password)
+INSERT INTO usuario (email, password)
 VALUES ('test@test.com', '1234');
